612b0067c857beca2f79f28439b83c5fc9a3d6ef
Jugveer-Sandher <jugveersandher@gmail.com>
2024-05-04 15:14:07 -0700

    part 1b

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>
 }
 
 const Part = ({ index, part }) => {
@@ -28,18 +28,20 @@ const Footer = ({ parts }) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14},
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7},
+      {name: 'State of a component', exercises: 14}
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Footer parts={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Footer parts={course.parts} />
     </>
   );
 }
